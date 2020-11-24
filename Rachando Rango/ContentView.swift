f495d2d99e6a334aaa177cf3ed70d2db35eb23ad
Ewerson Castelo <ewerson.castelo@gmail.com>
2020-11-23 20:46:34 -0400

    add Picker

--- Rachando Rango/ContentView.swift
+++ Rachando Rango/ContentView.swift
@@ -8,17 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    @State private var name = ""
+    
+    let studentys = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
         NavigationView {
             Form {
                 Section{
-                    Text("Hello, world!")
+                    TextField("Enter with your name", text: $name)
+                    Text("Hello, \(name) how are you?")
+                    Button("Tap Count: \(tapCount)") {
+                        self.tapCount += 1
+                    }
+                }
+                
+                Section {
+                    Text("Please, shoose your studenty")
+                        .foregroundColor(.orange)
+                    Picker("Select your studenty", selection: $selectedStudent){
+                        ForEach(0 ..< studentys.count){
+                            Text("\(self.studentys[$0])")
+                        }
+                    }
                 }
             }
             .navigationBarTitle(Text("Rachando Rango"), displayMode: .inline)
         }
     }
 }
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
